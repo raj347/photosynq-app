29532df2c6da619ce2312065d6e61cf270b6da30
Shekhar Patil <shekhar@venturit.com>
2015-11-03 18:59:47 +0530

    Fetching pre-selected protocols
    Pull to refresh pre-selected protocols from web
    Ticket #6661
    Hours !2.0

--- app/src/main/java/com/photosynq/app/QuickModeFragment.java
+++ app/src/main/java/com/photosynq/app/QuickModeFragment.java
@@ -5,9 +5,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,7 +28,11 @@ import android.widget.Toast;
 import com.photosynq.app.db.DatabaseHelper;
 import com.photosynq.app.http.PhotosynqResponse;
 import com.photosynq.app.model.AppSettings;
+import com.photosynq.app.model.Macro;
+import com.photosynq.app.model.Option;
 import com.photosynq.app.model.Protocol;
+import com.photosynq.app.model.Question;
+import com.photosynq.app.model.ResearchProject;
 import com.photosynq.app.utils.BluetoothService;
 import com.photosynq.app.utils.CommonUtils;
 import com.photosynq.app.utils.Constants;
@@ -32,13 +40,23 @@ import com.photosynq.app.utils.PrefUtils;
 import com.photosynq.app.utils.SyncHandler;
 import com.squareup.picasso.Picasso;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 
-public class QuickModeFragment extends Fragment implements PhotosynqResponse{
+public class QuickModeFragment extends Fragment implements PhotosynqResponse , SwipeRefreshLayout.OnRefreshListener{
 
     /**
      * The fragment argument representing the section number for this
@@ -49,6 +67,7 @@ public class QuickModeFragment extends Fragment implements PhotosynqResponse{
     private DatabaseHelper dbHelper;
     private ProtocolArrayAdapter arrayAdapter;
     private ListView protocolList;
+    private SwipeRefreshLayout mListViewContainer;
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -67,6 +86,9 @@ public class QuickModeFragment extends Fragment implements PhotosynqResponse{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_quick_mode, container, false);
+
+        mListViewContainer = (SwipeRefreshLayout) rootView.findViewById(R.id.swipeRefreshLayout_listView);
+        onCreateSwipeToRefresh(mListViewContainer);
 
         dbHelper = DatabaseHelper.getHelper(getActivity());
 
@@ -220,5 +242,155 @@ public class QuickModeFragment extends Fragment implements PhotosynqResponse{
 
             return convertView;
         }
+    }
+    private void onCreateSwipeToRefresh(SwipeRefreshLayout refreshLayout) {
+
+        refreshLayout.setOnRefreshListener(this);
+
+        refreshLayout.setColorScheme(
+                android.R.color.holo_blue_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_green_light,
+                android.R.color.holo_red_light);
+
+    }
+    @Override
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                String authToken = PrefUtils.getFromPrefs(getActivity().getApplicationContext(), PrefUtils.PREFS_AUTH_TOKEN_KEY, PrefUtils.PREFS_DEFAULT_VAL);
+                String email = PrefUtils.getFromPrefs(getActivity().getApplicationContext(), PrefUtils.PREFS_LOGIN_USERNAME_KEY, PrefUtils.PREFS_DEFAULT_VAL);
+                new DownloadPreSelectedProtocols().execute(getActivity().getApplicationContext(), Constants.PHOTOSYNQ_PRE_SEL_PROTOCOLS_LIST_URL
+                        + "&user_email=" + email + "&user_token="
+                        + authToken);
+            }
+        }, 10);
+    }
+
+    public class DownloadPreSelectedProtocols extends AsyncTask<Object, Object, String> {
+        @Override
+        protected String doInBackground(Object... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            Context context = (Context)uri[0];
+            HttpResponse response = null;
+            HttpGet getRequest;
+            String responseString = null;
+            if(!CommonUtils.isConnected(context))
+            {
+                return Constants.SERVER_NOT_ACCESSIBLE;
+            }
+            Log.d("PHTTPC", "in async task");
+            try {
+                Log.d("PHTTPC", "$$$$ URI"+uri[1]);
+                getRequest = new HttpGet((String) uri[1]);
+                Log.d("PHTTPC", "$$$$ Executing GET request");
+                response = httpclient.execute(getRequest);
+
+                if (null != response) {
+                    try {
+                        StatusLine statusLine = response.getStatusLine();
+                        if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
+                            ByteArrayOutputStream out = new ByteArrayOutputStream();
+                            response.getEntity().writeTo(out);
+                            out.close();
+                            responseString = out.toString();
+
+                            processResult(responseString);
+
+                        } else {
+                            //Closes the connection.
+                            response.getEntity().getContent().close();
+                            throw new IOException(statusLine.getReasonPhrase());
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            } catch (IOException e) {
+            }
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            // Notify swipeRefreshLayout that the refresh has finished
+            showAllProtocolList();
+            mListViewContainer.setRefreshing(false);
+
+        }
+
+        private void processResult(String result) {
+            JSONArray jArray;
+            DatabaseHelper db = DatabaseHelper.getHelper(getActivity());
+            if (null != result) {
+                if(result.equals(Constants.SERVER_NOT_ACCESSIBLE))
+                {
+                    if(null != getActivity()) {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(getActivity(), R.string.server_not_reachable, Toast.LENGTH_LONG).show();
+                            }
+
+                        });
+                    }
+                    return;
+                }
+
+                try {
+                    JSONObject resultJsonObject = new JSONObject(result);
+
+                    if (resultJsonObject.has("protocols")) {
+                        String newobj = resultJsonObject.getString("protocols");
+                        jArray = new JSONArray(newobj);
+                        for (int i = 0; i < jArray.length(); i++) {
+
+                            JSONObject obj = jArray.getJSONObject(i);
+                            String id = obj.getString("id");
+                            Protocol protocol = new Protocol(id,
+                                    obj.getString("name"),
+                                    obj.getString("protocol_json"),
+                                    obj.getString("description"),
+                                    obj.getString("macro_id"), "slug",
+                                    obj.getString("pre_selected"));
+                            db.updateProtocol(protocol);
+                        }
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            if(null != getActivity()) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+
+                            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+
+                            QuickModeFragment fragmentSelectProtocol = (QuickModeFragment) fragmentManager.findFragmentByTag(QuickModeFragment.class.getName());
+                            if (fragmentSelectProtocol != null) {
+                                fragmentSelectProtocol.onResponseReceived(Constants.SUCCESS);
+                            }
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+
+                });
+            }
+
+        }
+
     }
 }
